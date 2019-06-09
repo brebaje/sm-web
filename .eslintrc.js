9470b61defaee6214c0d91a5169f4ee0a63b6b15
Manuel Bravo <manuelbravo@gmail.com>
2019-06-09 03:08:51 +0200

    Adding custom eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
